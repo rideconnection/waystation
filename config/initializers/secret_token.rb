f8aa8b2a68ad050fbe612bf974f6629d66d37c7f
Kevin Chambers <kc@kevinchambers.com>
2011-06-28 11:32:08 -0700

    Rails 3.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waystation::Application.config.secret_token = 'fec0757b13edf71872ef63d77514d17c250a4a10a34b3f50e0bf086d1e7ad7a5f073dca26164b2e510b1db9ca3317970dbda89d2d0b028704684179b03fb9e5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Waystation::Application.config.session_store :cookie_store, :key => '_waystation_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Waystation::Application.config.session_store :active_record_store
