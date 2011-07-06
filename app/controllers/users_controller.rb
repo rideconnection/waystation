b2eaa77e3c27182ec41a2748d15377755bd5a923
Kevin Chambers <kc@kevinchambers.com>
2011-07-05 17:26:48 -0700

    Authentication configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_filter :login_required, :except => [:new, :create]
+  before_filter :login_required
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -8,23 +12,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Thank you for signing up! You are now logged in."
+      redirect_to users_path, :notice => "New user successfully created."
     else
       render :action => 'new'
     end
   end
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = current_user
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      redirect_to root_url, :notice => "Your profile has been updated."
+      redirect_to users_path, :notice => "New user successfully created."
     else
       render :action => 'edit'
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, :notice => "Successfully destroyed user."
   end
 end
