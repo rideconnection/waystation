4e2d79193f2720f9d939d80afa863413372053e3
Kevin Chambers <kc@kevinchambers.com>
2011-07-07 10:09:33 -0700

    Add 403 response for cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include ControllerAuthentication
   include Userstamp
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", :status => 403
+  end
 end
