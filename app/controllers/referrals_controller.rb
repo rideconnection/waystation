33a710979d52dda64245f345d4c8bd0c492f7cde
Kevin Chambers <kc@kevinchambers.com>
2011-07-07 15:53:55 -0700

    Cancan configuration for referrals

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -1,20 +1,17 @@
 class ReferralsController < ApplicationController
+  load_and_authorize_resource
   before_filter :login_required
 
   def index
-    @referrals = Referral.all
   end
 
   def show
-    @referral = Referral.find(params[:id])
   end
 
   def new
-    @referral = Referral.new
   end
 
   def create
-    @referral = Referral.new(params[:referral])
     if @referral.save
       redirect_to @referral, :notice => "Successfully created referral."
     else
@@ -23,11 +20,9 @@ class ReferralsController < ApplicationController
   end
 
   def edit
-    @referral = Referral.find(params[:id])
   end
 
   def update
-    @referral = Referral.find(params[:id])
     if @referral.update_attributes(params[:referral])
       redirect_to @referral, :notice  => "Successfully updated referral."
     else
@@ -36,13 +31,12 @@ class ReferralsController < ApplicationController
   end
 
   def destroy
-    @referral = Referral.find(params[:id])
     @referral.destroy
     redirect_to referrals_url, :notice => "Successfully destroyed referral."
   end
 
   def download
-    @referral = Referral.find(params[:id])
+    #@referral = Referral.find(params[:id])
     send_file @referral.ucr_form.path, :type => @referral.ucr_form_content_type, :disposition => 'inline', :x_sendfile => true
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.admin?
+      can :manage, :all
+    elsif user.user_type == 'Ride Connection User'
+      can :read, Referral
+    elsif user.user_type == 'Outside User'
+      can :manage, Referral, :created_by => user.id
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
