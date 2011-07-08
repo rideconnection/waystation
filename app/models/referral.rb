0780ba8633c21dd4aa818b4e6c00a2c481db1e72
Kevin Chambers <kc@kevinchambers.com>
2011-07-07 18:03:50 -0700

    Add details to referral list and show

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -4,6 +4,9 @@ class Referral < ActiveRecord::Base
                     :updater_attribute  => :updated_by
   #attr_accessible :first_name, :last_name, :customer_phone, :case_manager_name, :case_manager_email, :note
 
+  belongs_to :creator, :class_name => 'User', :foreign_key => :created_by
+  belongs_to :updater, :class_name => 'User', :foreign_key => :updated_by
+
   has_attached_file :ucr_form, 
       :path => ":rails_root/uploads/:class/:id/:basename.:extension",
       :url => "/referrals/:id/download"
