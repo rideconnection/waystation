b9446c96e6d2e35a5b69ccce6a1f68b5510329ce
Kevin Chambers <kc@kevinchambers.com>
2011-07-20 14:06:36 -0700

    moved exception notification settings to config file outside repository

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
 #Exception Notification
 Rails.application.config.middleware.use ExceptionNotifier,
-    :email_prefix => '[Waystation] ',
-    :sender_address => 'apps@rideconnection.org',
-    :exception_recipients => ['kchambers@rideconnection.org']
+    :email_prefix => APP_CONFIG[:exception_notification],
+    :sender_address => APP_CONFIG[:exception_sender_address],
+    :exception_recipients => APP_CONFIG[:exception_recipients]
