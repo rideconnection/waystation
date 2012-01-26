0a687828dad6d50373bedf1b4a83650188484408
Kevin Chambers <kc@kevinchambers.com>
2012-01-26 14:53:43 -0800

    Remove usr form requirement

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -16,7 +16,7 @@ class Referral < ActiveRecord::Base
   validates_presence_of :customer_phone
   validates_presence_of :case_manager_name
   validates_presence_of :case_manager_email
-  validates_attachment_presence :ucr_form
+  #validates_attachment_presence :ucr_form
   
   def name
     "#{last_name}, #{first_name}"
